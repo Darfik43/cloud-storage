04f9f1585bc2683353938d4ceaddc917d6865154
Darfik43 <darfik43@gmail.com>
2024-05-23 19:55:20 +0300

    Add custom method into repository

--- src/main/java/com/darfik/cloudstorage/user/AppUserRepository.java
+++ src/main/java/com/darfik/cloudstorage/user/AppUserRepository.java
@@ -1,8 +1,16 @@
 package com.darfik.cloudstorage.user;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Repository
 public interface AppUserRepository extends JpaRepository<AppUser, Long> {
+
+    @Transactional
+    @Query("SELECT user FROM AppUser user WHERE user.username = ?1")
+    Optional<AppUser> findAppUserByUsername(String username);
 }
