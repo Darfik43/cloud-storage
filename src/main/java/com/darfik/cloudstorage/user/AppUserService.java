91be3d3049677d08ba1f4e9b69af4254c9ba3afd
Darfik43 <darfik43@gmail.com>
2024-05-23 19:56:28 +0300

    Change addNewUser:
    - if user sends username that already in DB, then we throw the exception

--- src/main/java/com/darfik/cloudstorage/user/AppUserService.java
+++ src/main/java/com/darfik/cloudstorage/user/AppUserService.java
@@ -1,5 +1,6 @@
 package com.darfik.cloudstorage.user;
 
+import com.darfik.cloudstorage.mapper.AppUserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -9,7 +10,19 @@ import java.util.List;
 @AllArgsConstructor
 public class AppUserService {
     private final AppUserRepository appUserRepository;
+    private final AppUserMapper mapper;
+
     public List<AppUser> getUsers() {
         return appUserRepository.findAll();
+    }
+
+    public void addNewUser(AppUserRequest appUserRequest) {
+        appUserRepository.findAppUserByUsername(appUserRequest.getUsername())
+                .ifPresentOrElse((user) ->
+                        {
+                            throw new UserAlreadyExistsException("User already exists");
+                        },
+                        () -> appUserRepository.save(mapper.dtoToModel(appUserRequest))
+                );
     }
 }
