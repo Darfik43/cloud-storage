b08ada96ada7b79df3a9c2afc9fa50b81509c276
Darfik43 <darfik43@gmail.com>
2024-05-23 19:55:36 +0300

    Add DTO for UserRequest

---
+++ src/main/java/com/darfik/cloudstorage/user/AppUserRequest.java
@@ -0,0 +1,17 @@
+package com.darfik.cloudstorage.user;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class AppUserRequest {
+    @NotBlank (message = "username should not be null")
+    private String username;
+    @NotBlank (message = "password should not be null")
+    private String password;
+}
+
