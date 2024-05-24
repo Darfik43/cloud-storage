e8212ad63305275698d6e42a2be894377cd7c44c
Darfik43 <darfik43@gmail.com>
2024-05-24 23:19:57 +0300

    Add Custom ValidPassword annotation

---
+++ src/main/java/com/darfik/cloudstorage/security/PasswordConstraintValidator.java
@@ -0,0 +1,29 @@
+package com.darfik.cloudstorage.security;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.passay.*;
+
+import java.util.Arrays;
+public class PasswordConstraintValidator implements ConstraintValidator<ValidPassword, String> {
+    @Override
+    public void initialize(ValidPassword constraintAnnotation) {
+    }
+
+    @Override
+    public boolean isValid(String password, ConstraintValidatorContext constraintValidatorContext) {
+        PasswordValidator validator = new PasswordValidator(Arrays.asList(
+                new CharacterCharacteristicsRule(3, Arrays.asList(
+                        new CharacterRule(EnglishCharacterData.UpperCase, 1),
+                        new CharacterRule(EnglishCharacterData.LowerCase, 1),
+                        new CharacterRule(EnglishCharacterData.Digit, 1),
+                        new CharacterRule(EnglishCharacterData.Special, 1)
+                )),
+                new LengthRule(8, 30)
+        )
+        );
+
+        RuleResult result = validator.validate(new PasswordData(password));
+        return result.isValid();
+    }
+}
---
+++ src/main/java/com/darfik/cloudstorage/security/ValidPassword.java
@@ -0,0 +1,16 @@
+package com.darfik.cloudstorage.security;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.FIELD, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordConstraintValidator.class)
+@Documented
+public @interface ValidPassword {
+    String message() default "Invalid Password";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
