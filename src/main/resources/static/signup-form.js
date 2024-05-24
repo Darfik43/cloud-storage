422d4c4877c89bf28ab65a7d9383a56f73e66257
Darfik43 <darfik43@gmail.com>
2024-05-24 23:19:34 +0300

    Add js scripts for sign-up form

---
+++ src/main/resources/static/signup-form.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    $('#passwordRequirementsBtn').click(function () {
+        $('#passwordRequirementsModal').modal('show');
+    });
+
+    $('form input').on('input', function () {
+        var totalFields = $('form input').length;
+        var completedFields = $('form input').filter(function () {
+            return $(this).val().trim() !== '';
+        }).length;
+        var progress = (completedFields / totalFields) * 100;
+        $('.progress-bar').css('width', progress + '%').attr('aria-valuenow', progress);
+    });
+});
+$(document).ready(function(){
+    $('#togglePassword').click(function(){
+        var passwordField = $('#password');
+        var passwordFieldType = passwordField.attr('type');
+        var toggleIcon = $('#togglePassword i');
+
+        if(passwordFieldType === 'password'){
+            passwordField.attr('type', 'text');
+            toggleIcon.removeClass('fa-eye-slash').addClass('fa-eye');
+        } else {
+            passwordField.attr('type', 'password');
+            toggleIcon.removeClass('fa-eye').addClass('fa-eye-slash');
+        }
+    });
+});
