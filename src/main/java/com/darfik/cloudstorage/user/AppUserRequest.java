db00c94fbbd9cd3747169b666580a62aeb592e63
Darfik43 <darfik43@gmail.com>
2024-05-24 23:21:03 +0300

    Edit username to email

--- src/main/java/com/darfik/cloudstorage/user/AppUserRequest.java
+++ src/main/java/com/darfik/cloudstorage/user/AppUserRequest.java
@@ -1,5 +1,8 @@
 package com.darfik.cloudstorage.user;
 
+import com.darfik.cloudstorage.security.PasswordMatches;
+import com.darfik.cloudstorage.security.ValidPassword;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
@@ -9,13 +12,18 @@ import lombok.*;
 @NoArgsConstructor
 @Getter
 @Setter
+@PasswordMatches
 public class AppUserRequest {
-    @NotBlank (message = "username should not be null")
+    @NotBlank (message = "email should not be null")
     @NotNull
+    @Email(message = "Invalid email", regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
     @NotBlank (message = "password should not be null")
     @NotNull
+    @ValidPassword
     private String password;
+    @NotBlank (message = "Confirm should not be null")
+    @NotNull (message = "Confirm should not be null")
     private String matchingPassword;
 }
 
