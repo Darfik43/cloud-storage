87c21b1e73122e6d557ac1712ed7be438a8465a6
Darfik43 <darfik43@gmail.com>
2024-05-24 23:25:18 +0300

    AppUserService edit:
    - Separate addNewUser to 2 methods

--- src/main/java/com/darfik/cloudstorage/user/AppUserService.java
+++ src/main/java/com/darfik/cloudstorage/user/AppUserService.java
@@ -3,10 +3,12 @@ package com.darfik.cloudstorage.user;
 import com.darfik.cloudstorage.mapper.AppUserMapper;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
+@Transactional
 @AllArgsConstructor
 public class AppUserService {
     private final AppUserRepository appUserRepository;
@@ -17,12 +19,14 @@ public class AppUserService {
     }
 
     public void addNewUser(AppUserRequest appUserRequest) {
-        appUserRepository.findAppUserByUsername(appUserRequest.getEmail())
-                .ifPresentOrElse((user) ->
-                        {
-                            throw new UserAlreadyExistsException("User already exists");
-                        },
-                        () -> appUserRepository.save(mapper.dtoToModel(appUserRequest))
-                );
+        if (!userExists(appUserRequest.getEmail())) {
+            appUserRepository.save(mapper.dtoToModel(appUserRequest));
+        } else {
+            throw new UserAlreadyExistsException("User already exists" + appUserRequest.getEmail());
+        }
+    }
+
+    private boolean userExists(String email) {
+        return appUserRepository.findAppUserByEmail(email).isPresent();
     }
 }
