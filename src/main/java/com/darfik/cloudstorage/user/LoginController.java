e2ea0215fdd253ad74d749b9ad29ae28e6c4a0de
Darfik43 <darfik43@gmail.com>
2024-05-25 22:36:20 +0300

    feat(controller): add LoginController

---
+++ src/main/java/com/darfik/cloudstorage/user/LoginController.java
@@ -0,0 +1,19 @@
+package com.darfik.cloudstorage.user;
+
+
+import ch.qos.logback.core.model.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+
+    @GetMapping
+    public String showLoginForm() {
+        return "login";
+    }
+}
