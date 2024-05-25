9b9144ade963e465eabe6056b0f920350da4c105
Darfik43 <darfik43@gmail.com>
2024-05-25 22:35:26 +0300

    style(DTO): add whitespaces

--- src/main/java/com/darfik/cloudstorage/user/AppUserRequest.java
+++ src/main/java/com/darfik/cloudstorage/user/AppUserRequest.java
@@ -14,14 +14,17 @@ import lombok.*;
 @Setter
 @PasswordMatches
 public class AppUserRequest {
+
     @NotBlank (message = "email should not be null")
     @NotNull
     @Email(message = "Invalid email", regexp = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$")
     private String email;
+
     @NotBlank (message = "password should not be null")
     @NotNull
     @ValidPassword
     private String password;
+
     @NotBlank (message = "Confirm should not be null")
     @NotNull (message = "Confirm should not be null")
     private String matchingPassword;
