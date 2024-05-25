0bfaef5f0a2bcebe986a82aedc8a7503d79729b4
Darfik43 <darfik43@gmail.com>
2024-05-26 01:11:42 +0300

    feat(test): add integration tests for UserService

---
+++ src/test/java/com/darfik/cloudstorage/security/TestWebSecurityConfiguration.java
@@ -0,0 +1,30 @@
+package com.darfik.cloudstorage.security;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@TestConfiguration
+public class TestWebSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(registry -> {
+                    registry.requestMatchers("/home", "/signup/**").permitAll();
+                    registry.requestMatchers("/webjars/**").permitAll();
+                    registry.requestMatchers("/signup-form.js", "/signup-form.css", "/password-validator.js").permitAll();
+                    registry.requestMatchers("/admin/**").hasRole("ADMIN");
+                    registry.requestMatchers("/user/**").hasRole("USER");
+                    registry.anyRequest().authenticated();
+                })
+                .formLogin(httpSecurityFormLoginConfigurer ->
+                        httpSecurityFormLoginConfigurer.loginPage("/login").usernameParameter("email")
+                                .defaultSuccessUrl("/home").permitAll());
+
+        return httpSecurity.build();
+    }
+}
---
+++ src/test/java/com/darfik/cloudstorage/service/UserServiceIntegrationTest.java
@@ -0,0 +1,72 @@
+package com.darfik.cloudstorage.service;
+
+
+import com.darfik.cloudstorage.dto.RegistrationDto;
+import com.darfik.cloudstorage.exception.UserAlreadyExistsException;
+import com.darfik.cloudstorage.model.AppUser;
+import com.darfik.cloudstorage.repository.AppUserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertNotEquals;
+
+@SpringBootTest
+@Testcontainers
+public class UserServiceIntegrationTest {
+
+    @Container
+    private static final PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:latest");
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private AppUserRepository appUserRepository;
+
+    @BeforeEach
+    void beforeEach() {
+        appUserRepository.deleteAll();
+    }
+
+    @Test
+    void register_shouldSaveUserInDatabase() {
+        var registrationDto = new RegistrationDto();
+        registrationDto.setEmail("user@gmail.com");
+        registrationDto.setPassword("password");
+
+        userService.registerNewUser(registrationDto);
+
+        Optional<AppUser> user = appUserRepository.findByEmail("user@gmail.com");
+        assertTrue(user.isPresent(), "User exists in database");
+        user.ifPresent(u -> {
+            assertEquals("Emails don't match","user@gmail.com", u.getEmail());
+            assertNotEquals("Password should be hashed", u.getPassword(), registrationDto.getPassword());
+        });
+    }
+
+    @Test
+    void register_sameUserTwice_shouldThrowException() {
+        var registrationDto = new RegistrationDto();
+        registrationDto.setEmail("user@gmail.com");
+        registrationDto.setPassword("password");
+
+        userService.registerNewUser(registrationDto);
+
+        assertThrows(
+                UserAlreadyExistsException.class,
+                () -> userService.registerNewUser(registrationDto)
+        );
+        assertEquals("User should be saved only once", appUserRepository.findAll().size(), 1);
+    }
+}
