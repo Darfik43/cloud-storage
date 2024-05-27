80f46f145d31a49fcf2b8919ba4d84bc0949ea69
Darfik43 <darfik43@gmail.com>
2024-05-28 00:15:40 +0300

    refactor(dto): add new parameters to home url

--- src/main/java/com/darfik/cloudstorage/controller/HomeController.java
+++ src/main/java/com/darfik/cloudstorage/controller/HomeController.java
@@ -1,15 +1,32 @@
 package com.darfik.cloudstorage.controller;
 
+import com.darfik.cloudstorage.model.AppUser;
+import com.darfik.cloudstorage.service.AppFileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller("/")
 @Tag(name = "Home controller", description = "Home API")
+@RequiredArgsConstructor
 public class HomeController {
 
+    private final AppFileService appFileService;
+
     @GetMapping()
-    public String showHomePage() {
+    public String showHomePage(
+            @AuthenticationPrincipal User user,
+            @RequestParam(value ="path", required = false, defaultValue = "") String path,
+            Model model) {
+
+        model.addAttribute(appFileService.getUserFiles(user.getUsername(), path));
         return "home";
     }
 
