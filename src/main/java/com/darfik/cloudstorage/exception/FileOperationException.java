d3eb8b82f2869fba729cbd3f254c2fe620266866
Darfik43 <darfik43@gmail.com>
2024-05-28 00:13:06 +0300

    refactoring(dto): add new general FileOperationException to handle all errors while processing any actions with files

---
+++ src/main/java/com/darfik/cloudstorage/exception/FileOperationException.java
@@ -0,0 +1,8 @@
+package com.darfik.cloudstorage.exception;
+
+public class FileOperationException extends RuntimeException {
+
+    public FileOperationException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/darfik/cloudstorage/exception/FileUploadException.java
+++
@@ -1,7 +0,0 @@
-package com.darfik.cloudstorage.exception;
-
-public class FileUploadException extends RuntimeException {
-
-    public FileUploadException(String message) { super(message); }
-
-}
