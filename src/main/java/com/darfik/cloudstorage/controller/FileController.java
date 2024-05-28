040bc4e8826c0e1063a747bd5a05dedd76a5c13a
Darfik43 <darfik43@gmail.com>
2024-05-28 11:28:27 +0300

    style(controller)

--- src/main/java/com/darfik/cloudstorage/controller/FileController.java
+++ src/main/java/com/darfik/cloudstorage/controller/FileController.java
@@ -9,7 +9,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
