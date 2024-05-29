457718e0e5141cd6cd9a69d629cbac6f4bf22d90
Darfik43 <darfik43@gmail.com>
2024-05-30 00:32:51 +0300

    feat(dto): add new end point to render home page

--- src/main/java/com/darfik/cloudstorage/controller/HomeController.java
+++ src/main/java/com/darfik/cloudstorage/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.darfik.cloudstorage.controller;
 
+import com.darfik.cloudstorage.dto.AppFileDto;
 import com.darfik.cloudstorage.service.AppFileService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -8,23 +9,26 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller("/")
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
 @Tag(name = "Home controller", description = "Home API")
 @RequiredArgsConstructor
 public class HomeController {
 
     private final AppFileService appFileService;
 
-    @GetMapping()
-    public String showHomePage(
-            @AuthenticationPrincipal User user,
-            @RequestParam(value = "path", required = false,
-                    defaultValue = "") String path,
-            Model model) {
+    @GetMapping("/")
+    public String getFiles(@RequestParam(required = false) String path, @AuthenticationPrincipal User user, Model model) {
+        String currentPath = path != null ? path : "";
+        List<AppFileDto> folderContent = appFileService.getUserFiles(user.getUsername(), currentPath);
 
-        model.addAttribute(appFileService.getUserFiles(user.getUsername(), path));
+        model.addAttribute("folderContent", folderContent);
+        model.addAttribute("currentPath", currentPath);
         return "home";
     }
 
