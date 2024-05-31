872f5ae9064ebc545da9b54cd917c094ea78e3f6
Darfik43 <darfik43@gmail.com>
2024-05-31 22:36:44 +0300

    refactor(dto): add validation

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderRenameRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderRenameRequest.java
@@ -1,4 +1,14 @@
 package com.darfik.cloudstorage.domain.s3storage.folder;
 
-public record FolderRenameRequest(String currentName, String newName) {
+import jakarta.validation.constraints.NotBlank;
+
+public record FolderRenameRequest(
+
+        @NotBlank(message = "Current name can't be blank")
+        String currentName,
+
+        @NotBlank(message = "New name can't be blank")
+        String newName
+
+) {
 }
--- src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderUploadRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderUploadRequest.java
@@ -1,7 +1,13 @@
 package com.darfik.cloudstorage.domain.s3storage.folder;
 
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
-public record FolderUploadRequest(List<MultipartFile> files) {
+public record FolderUploadRequest(
+
+        @NotBlank(message = "Folder can't be null")
+        List<MultipartFile> files
+
+) {
 }
