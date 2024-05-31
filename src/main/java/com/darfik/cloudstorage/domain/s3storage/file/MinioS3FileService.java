0bb1fd6bf1ad4b9eabc5b417288fffccf54b962a
Darfik43 <darfik43@gmail.com>
2024-05-31 22:22:04 +0300

    refactor(service): refactor service class

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/file/MinioS3FileService.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/file/MinioS3FileService.java
@@ -24,73 +24,10 @@ public class MinioS3FileService implements S3FileService {
     @Override
     public void uploadFile(FileUploadRequest fileUploadRequest, String owner) {
         MultipartFile multipartFile = fileUploadRequest.file();
-
         String fileName =
                 getUserFolderPrefix(owner) + fileUploadRequest.file().getOriginalFilename();
-        InputStream inputStream;
-        try {
-            inputStream = multipartFile.getInputStream();
-            minioClient.putObject(PutObjectArgs.builder()
-                    .stream(inputStream, inputStream.available(), -1)
-                    .bucket(minioProperties.getBucket())
-                    .object(fileName)
-                    .build());
-        } catch (Exception e) {
-            throw new FileOperationException("File upload failed" + e.getMessage());
-        }
-    }
 
-    @Override
-    public void renameFile(FileRenameRequest fileRenameRequest, String owner) {
-        try {
-            minioClient.copyObject(CopyObjectArgs.builder()
-                    .bucket(minioProperties.getBucket())
-                    .object(getUserFolderPrefix(owner + fileRenameRequest.newName()))
-                    .source(CopySource.builder()
-                            .bucket(minioProperties.getBucket())
-                            .object(getUserFolderPrefix(owner + fileRenameRequest.currentName()))
-                            .build())
-                    .build());
-        } catch (Exception e) {
-            throw new FileOperationException("File rename failed" + e.getMessage());
-        }
-
-        FileDeleteRequest fileDeleteRequest = new FileDeleteRequest(fileRenameRequest.path());
-
-        deleteFile(fileDeleteRequest, owner);
-    }
-
-    @Override
-    public List<FileDto> getUserFiles(String email, String folder,
-                                      boolean recursive) {
-        Iterable<Result<Item>> results =
-                minioClient.listObjects(ListObjectsArgs.builder()
-                        .bucket(minioProperties.getBucket())
-                        .prefix(getUserFolderPrefix(email) + folder)
-                        .recursive(recursive)
-                        .build());
-
-        List<FileDto> files = new ArrayList<>();
-
-        results.forEach(result -> {
-            try {
-                Item item = result.get();
-                String path = folder;
-                String name =
-                        item.objectName().substring((getUserFolderPrefix(email) + folder).length());
-                FileDto fileDto = new FileDto(
-                        email,
-                        item.isDir(),
-                        path,
-                        name
-                );
-                files.add(fileDto);
-            } catch (Exception e) {
-                throw new FileOperationException("Files get failed" + e.getMessage());
-            }
-        });
-
-        return files;
+        putObject(multipartFile, fileName);
     }
 
     @Override
@@ -105,19 +42,83 @@ public class MinioS3FileService implements S3FileService {
         }
     }
 
+    @Override
+    public void renameFile(FileRenameRequest fileRenameRequest, String owner) {
+        copyObject(fileRenameRequest, owner);
+
+        deleteFile(new FileDeleteRequest(fileRenameRequest.path()), owner);
+    }
+
+
+    @Override
+    public List<FileDto> getUserFiles(String email, String path, boolean recursive) {
+        Iterable<Result<Item>> results = getListObjects(email, path, recursive);
+
+        return convertResultToDto(results, email, path);
+    }
+
+    private void copyObject(FileRenameRequest fileRenameRequest, String owner) {
+        try {
+            minioClient.copyObject(CopyObjectArgs.builder()
+                    .bucket(minioProperties.getBucket())
+                    .object(getUserFolderPrefix(owner + fileRenameRequest.newName()))
+                    .source(CopySource.builder()
+                            .bucket(minioProperties.getBucket())
+                            .object(getUserFolderPrefix(owner + fileRenameRequest.currentName()))
+                            .build())
+                    .build());
+        } catch (Exception e) {
+            throw new FileOperationException("File rename failed: " + e.getMessage());
+        }
+    }
+
+    private void putObject(MultipartFile multipartFile, String fileName) {
+        InputStream inputStream;
+        try {
+            inputStream = multipartFile.getInputStream();
+            minioClient.putObject(PutObjectArgs.builder()
+                    .stream(inputStream, inputStream.available(), -1)
+                    .bucket(minioProperties.getBucket())
+                    .object(fileName)
+                    .build());
+        } catch (Exception e) {
+            throw new FileOperationException("File upload failed: " + e.getMessage());
+        }
+    }
+
+    private List<FileDto> convertResultToDto(Iterable<Result<Item>> results, String email, String path) {
+        List<FileDto> files = new ArrayList<>();
+
+        results.forEach(result -> {
+            try {
+                Item item = result.get();
+                String name =
+                        item.objectName().substring((getUserFolderPrefix(email) + path).length());
+                FileDto fileDto = new FileDto(
+                        email,
+                        item.isDir(),
+                        path,
+                        name
+                );
+                files.add(fileDto);
+            } catch (Exception e) {
+                throw new FileOperationException("Can't get a list of your files: " + e.getMessage());
+            }
+        });
+
+        return files;
+    }
+
+    private Iterable<Result<Item>> getListObjects(String email, String path, boolean recursive) {
+        return minioClient.listObjects(ListObjectsArgs.builder()
+                .bucket(minioProperties.getBucket())
+                .prefix(getUserFolderPrefix(email) + path)
+                .recursive(recursive)
+                .build());
+    }
+
     private String getUserFolderPrefix(String email) {
         return "user-" + userService.getUserIdByEmail(email) + "-files/";
     }
 
-    private String getPath(String name, String email) {
-        return name.substring(getUserFolderPrefix(email).length(),
-                name.lastIndexOf("/") + 1);
-    }
-
-    private String getFileName(String path, String email) {
-        return path.endsWith("/") ?
-                path.substring(getUserFolderPrefix(email).length(),
-                        path.lastIndexOf("/") + 1) :
-                path.substring(path.lastIndexOf("/") + 1);
-    }
 }
