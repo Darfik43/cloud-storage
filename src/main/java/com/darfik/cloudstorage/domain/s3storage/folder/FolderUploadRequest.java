c2a4dd0e7ee5a37d542350f1bc1345660df6d290
Darfik43 <darfik43@gmail.com>
2024-06-01 01:58:31 +0300

    refactor(controller): NotBlank doesn't annotate files, cuz it can't

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderUploadRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/folder/FolderUploadRequest.java
@@ -1,12 +1,10 @@
 package com.darfik.cloudstorage.domain.s3storage.folder;
 
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 public record FolderUploadRequest(
 
-        @NotBlank(message = "Folder can't be null")
         List<MultipartFile> files
 
 ) {
