9b00443206074a9155b7c960bae8c621b302ed47
Darfik43 <darfik43@gmail.com>
2024-05-31 22:20:44 +0300

    refactor(dto): FileDto is a record class

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileDto.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileDto.java
@@ -1,21 +1,4 @@
 package com.darfik.cloudstorage.domain.s3storage.file;
 
-import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.Setter;
-
-@AllArgsConstructor
-@NotBlank
-@Getter
-@Setter
-@EqualsAndHashCode
-public class FileDto {
-
-    private String owner;
-    private boolean isDir;
-    private String path;
-    private String name;
-
+public record FileDto(String owner, boolean isDir, String path, String name) {
 }
