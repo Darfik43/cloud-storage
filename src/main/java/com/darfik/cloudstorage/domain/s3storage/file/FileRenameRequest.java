2f8ee63e130ed78c0dc6cee2aaff36fbb355df56
Darfik43 <darfik43@gmail.com>
2024-05-31 22:21:04 +0300

    style(dto): add tabulations

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileRenameRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileRenameRequest.java
@@ -1,12 +1,17 @@
 package com.darfik.cloudstorage.domain.s3storage.file;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-public record FileRenameRequest(
-        @NotBlank(message = "Current name can't be blank") String currentName,
-        @NotBlank(message = "New name can't be blank") String newName,
-        @NotBlank(message = "Can't get the path of the file") String path) {
 
+public record FileRenameRequest(
+
+        @NotBlank(message = "Current name can't be blank")
+        String currentName,
+
+        @NotBlank(message = "New name can't be blank")
+        String newName,
+
+        @NotBlank(message = "Can't get the path of the file")
+        String path
+
+) {
 }
