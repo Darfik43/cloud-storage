3e8e4774e84cba331791cf910bf34fc879b13218
Darfik43 <darfik43@gmail.com>
2024-05-31 22:37:39 +0300

    refactor(dto): add validation

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/file/SearchRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/file/SearchRequest.java
@@ -1,7 +1,10 @@
 package com.darfik.cloudstorage.domain.s3storage.file;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record SearchRequest(
 
+        @NotBlank(message = "Search request can't be null")
         String searchTerm
 
 ) {
