05a4adb7e9376ef796f89eb12e18d3f8e708ed57
Darfik43 <darfik43@gmail.com>
2024-06-03 19:46:26 +0300

    refactor(dto): request doesn't require path not to be null

--- src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileDeleteRequest.java
+++ src/main/java/com/darfik/cloudstorage/domain/s3storage/file/FileDeleteRequest.java
@@ -4,7 +4,6 @@ import jakarta.validation.constraints.NotBlank;
 
 public record FileDeleteRequest(
 
-        @NotBlank(message = "Failed while getting file path")
         String path,
 
         @NotBlank(message = "Can't delete nothing")
